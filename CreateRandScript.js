ff1ff6a7d119bd399b1123bf532fec301bbb2c37
murray-carolynA <65218209+murray-carolynA@users.noreply.github.com>
2020-10-08 00:19:50 -0700

    Update CreateRandScript.js
    Additional comments made for clarity in the future

--- CreateRandScript.js
+++ CreateRandScript.js
@@ -1,4 +1,4 @@
-//function to get n random non-repeating elements from an array
+//function to get n random non-repeating elements from an array-- I didn't write this, got it online. 
 	function getRandom(arr, n) {
 	var result = new Array(n),
 	len = arr.length,
@@ -14,36 +14,36 @@
 	return result;
 	}
 
-//type your URLs here one after another in quotes e.g. ['word1', 'word2', 'word3]
-	//Organized alphabetically within gender.
+	//type your URLs here one after another in quotes e.g. ['word1', 'word2', 'word3]
+	//Organize them when you put them in here! 
 	
 	URLlist =  [];//put urls here
 						 
 	
-	//get order for each list.
+	//Create a list of the length of your stimulus counts. 
 	mat1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
-    sifiOrd = []
-    sifiURLs = []
+    	sifiOrd = []
+    	sifiURLs = []
 	
-	for (i=0; i<=14;i++) {
-        //Create a larger list of numbers
-        newMat = getRandom(mat1,24);
-        sifiOrd= sifiOrd.concat(newMat);
-    };
+	for (i=0; i<=14;i++) { //This is hard-coded to 14 because I wanted 15 repetitions of each stimulus. Change for your experiments. 
+        	//Create a larger list of numbers
+        	newMat = getRandom(mat1,24);
+        	sifiOrd= sifiOrd.concat(newMat);
+    	};
     
-    sifiOrd = getRandom(sifiOrd,360)
+    	sifiOrd = getRandom(sifiOrd,360) //Rearrange the full trial set randomly. 
 
-    for (i=0; i<=sifiOrd.length; i++){
-        //push the URL associated w/ each number to the URL
-        sifiURLs.push(URLlist[sifiOrd[i]])
-    }
+        for (i=0; i<=sifiOrd.length; i++){ //Now assign URL to position matching each number in your trial set. 
+        	//push the URL associated w/ each number to the URL
+        	sifiURLs.push(URLlist[sifiOrd[i]])
+        }
 
 	//Embed data!	
 	for (i=0; i<=sifiURLs.length; i++) {
-		itemString = 'sifi'+(i+1).toString();
+		itemString = 'sifi'+(i+1).toString(); //This is the name of your embedded data for each item in the array
 		
-		Qualtrics.SurveyEngine.setEmbeddedData( itemString, sifiURLs[i]);
+		Qualtrics.SurveyEngine.setEmbeddedData( itemString, sifiURLs[i]); //assign item in the URL array to embedded data. 
 	};
 	
 	
